2746a0eb3036d823912a27a42cb45fa9424b6195
Partha <parthapipu@gmail.com>
2022-04-05 18:16:42 +0530

    created the service class

--- HealthCareSystem/src/main/java/com/cg/hcs/service/IDiagnosticTestService.java
+++ HealthCareSystem/src/main/java/com/cg/hcs/service/IDiagnosticTestService.java
@@ -2,30 +2,12 @@ package com.cg.hcs.service;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.cg.hcs.model.DiagnosticTest;
 
-public interface IDiagnosticTestService extends JpaRepository<DiagnosticTest, Integer> {
-	public static List<DiagnosticTest> getAllTest(){
-		return null;	
-	}
-
-	public static DiagnosticTest addNewTest(DiagnosticTest test) {
-		return test;
-		
-	}
-	public static List<DiagnosticTest> getTestsOfDiagnosticTest(){
-		return null;
-		
-	}
-	public static DiagnosticTest updateTestDetail(DiagnosticTest test) {
-		return test;
-		
-	}
-	public static DiagnosticTest removeTestFromDiagnosticCenter(int centerid,DiagnosticTest test) {
-		return test;
-	
-
-}
+public interface IDiagnosticTestService{
+	public List<DiagnosticTest> getAllTest();
+	public DiagnosticTest addNewTest(DiagnosticTest test);
+	public List<DiagnosticTest> getTestsOfDiagnosticTest();
+	public DiagnosticTest updateTestDetail(DiagnosticTest test);
+	public DiagnosticTest removeTestFromDiagnosticCenter(int centerid,DiagnosticTest test);
 }
