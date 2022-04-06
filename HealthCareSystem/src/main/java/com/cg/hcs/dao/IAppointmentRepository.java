21d1f288e8c05385c134d4f197eb9a6c73001e54
Partha <parthapipu@gmail.com>
2022-04-06 20:17:25 +0530

    modified repository classes of center and test

--- HealthCareSystem/src/main/java/com/cg/hcs/dao/IAppointmentRepository.java
+++ HealthCareSystem/src/main/java/com/cg/hcs/dao/IAppointmentRepository.java
@@ -12,11 +12,13 @@ import com.cg.hcs.model.Appointment;
 @Repository
 public interface IAppointmentRepository extends JpaRepository<Appointment, Integer> {
 	
-	@Query("select a from appointment_tbl a where a.patient.name=?1")
+	@Query("select a from appointment_tbl a join a.patient p where p.name=?1")
 	public Set<Appointment> viewAppointments(String patientName);
 	
 	//doubt
-	@Query("Select a from appointment_tbl a join a.diagnosticTests t where a.diagnosticCenter.id=?1 and t.testName=?2")
+	@Query("Select a from appointment_tbl a join a.diagnosticTests t "
+			+ "join a.diagnosticCenter c "
+			+ "where c.id=?1 and t.testName=?2 a.approvalStatus=?3")
 	public List<Appointment> getAppointmentList(int centerId,String test,int status);
 }
 
