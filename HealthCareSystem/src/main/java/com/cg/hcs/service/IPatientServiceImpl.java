3f38457e6b8e1d3c6601febea83d43e40e92a09b
PRAGATI <pragatijha2k@gmail.com>
2022-04-08 14:48:51 +0530

    updated IpatientServiceImpl

--- HealthCareSystem/src/main/java/com/cg/hcs/service/IPatientServiceImpl.java
+++ HealthCareSystem/src/main/java/com/cg/hcs/service/IPatientServiceImpl.java
@@ -3,52 +3,79 @@ package com.cg.hcs.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.cg.hcs.dao.IDiagnosticTestRepository;
 import com.cg.hcs.dao.IPatientRepository;
+import com.cg.hcs.exception.AppointmentException;
+import com.cg.hcs.exception.DiagnosticCenterException;
+import com.cg.hcs.exception.PatientException;
+import com.cg.hcs.model.Appointment;
+import com.cg.hcs.model.DiagnosticCenter;
+import com.cg.hcs.model.DiagnosticTest;
 import com.cg.hcs.model.Patient;
 import com.cg.hcs.model.TestResult;
 @Service
 public class IPatientServiceImpl implements IPatientService{
 	@Autowired
 	IPatientRepository repo;
-	@Override
-	public Patient registerPatient(Patient patient) {
-		// TODO Auto-generated method stub
-		repo.save(patient);
-		return patient;
-		
-		
-	}
 
 	@Override
-	public Patient updatePatientDetails(Patient patient) {
-		// TODO Auto-generated method stub
-		if(repo.existsById(patient.getPatientid())) {
-			repo.save(patient);
-			return patient;
+	public ResponseEntity<Patient> registerPatient(Patient patient) throws PatientException {
+		if (repo.existsById(patient.getPatientid())) {
+			throw new PatientException("Patient with the given Id already Exists");
 		}
-		return null;
+		repo.save(patient);
+		return new ResponseEntity<Patient>(patient, HttpStatus.OK);
 	}
 
 	@Override
-	public Patient viewPatient(String patientUserName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<TestResult> getAllTestResult(String patientUserName) {
-		// TODO Auto-generated method stub
-		return repo.getAllTestResult(patientUserName);
+	public ResponseEntity<Patient> updatePatientDetails(Patient patient) throws PatientException {
+		if (repo.existsById(patient.getPatientid())) {
+			repo.save(patient);
+			return new ResponseEntity<Patient>(patient, HttpStatus.OK);
+		}
+		throw new PatientException("Patient with the given Id doesn't Exists");
 		
 	}
 
 	@Override
-	public TestResult viewTestResult(int TestresultId) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<Patient> viewPatient(String patientUserName) throws PatientException {
+		Patient pat=new Patient();
+		if (repo.existsById(pat.getPatientid())) {
+			repo.save(pat);
+			return new ResponseEntity<Patient>(pat, HttpStatus.OK);
+		}
+		throw new PatientException("Patient with the given Id doesn't Exists");
+		
+		
 	}
+
+	@Override
+	public ResponseEntity<ResponseEntity<Patient>> getAllTestResult(String patientUserName) throws PatientException {
+		// TODO Auto-generated method stub
+		if (repo.findAll().size() == 0) {
+			throw new PatientException("No Patient is available");
+		}
+		return new ResponseEntity<ResponseEntity<Patient>>((ResponseEntity<Patient>) repo.findAll(), HttpStatus.OK);
+		
+	}
+
+	@Override
+	public ResponseEntity<TestResult> viewTestResult(int TestresultId) throws PatientException {
+		if (repo.existsById(TestresultId)) {
+			throw new PatientException("Patient with the given Id doesn't Exists");
+		}
+		TestResult test = repo.viewTestResult(TestresultId);
+		if (test == null) {
+			throw new PatientException("Test with the given name doesn't Exists");
+		}
+		return new ResponseEntity<TestResult>(test, HttpStatus.OK);
+		
+	}
+	
+	
 
 }
