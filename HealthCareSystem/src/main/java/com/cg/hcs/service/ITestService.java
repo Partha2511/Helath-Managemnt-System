b1c55eb4d771aa59b33371d92f9019fd2204ec15
Purabi <purabimohanty.official@gmail.com>
2022-04-08 15:10:41 +0530

    Completed testresult and testservice functionalities

--- HealthCareSystem/src/main/java/com/cg/hcs/service/ITestService.java
+++ HealthCareSystem/src/main/java/com/cg/hcs/service/ITestService.java
@@ -1,11 +1,16 @@
 package com.cg.hcs.service;
 
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.cg.hcs.exception.TestException;
 import com.cg.hcs.model.DiagnosticTest;
 
+
 public interface ITestService {
-	public DiagnosticTest addTest(DiagnosticTest test);
-	public DiagnosticTest updateTest(DiagnosticTest test);
-	public DiagnosticTest removeTest(DiagnosticTest test);
-	public List<DiagnosticTest> viewAllTest(String criteria);
+	public ResponseEntity<DiagnosticTest> addTest(DiagnosticTest test) throws TestException;
+	public ResponseEntity<DiagnosticTest> updateTest(DiagnosticTest test)throws TestException;
+	public ResponseEntity<DiagnosticTest> removeTest(DiagnosticTest test) throws TestException;
+	public ResponseEntity<List<DiagnosticTest>> viewAllTest(String criteria)throws TestException;
 }
--- HealthCareSystem/src/main/java/com/cg/hcs/service/ITestServiceImpl.java
+++ HealthCareSystem/src/main/java/com/cg/hcs/service/ITestServiceImpl.java
@@ -3,11 +3,15 @@ package com.cg.hcs.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
 import com.cg.hcs.dao.ITestRepository;
+import com.cg.hcs.exception.TestException;
 import com.cg.hcs.model.DiagnosticTest;
+
 
 @Service
 public class ITestServiceImpl implements ITestService{
@@ -15,32 +19,41 @@ public class ITestServiceImpl implements ITestService{
 	ITestRepository repo;
 	
 	@Autowired
-	public DiagnosticTest addTest(DiagnosticTest test) {
+	public  ResponseEntity<DiagnosticTest> addTest(DiagnosticTest test) throws TestException{
+		if(repo.existsById(test.getId())) {
+			throw new TestException("Test with the given Id already Exists");
+		}
 		repo.save(test);
-		return test;
+		return new ResponseEntity<DiagnosticTest>(test, HttpStatus.OK);
+		
 	}
 	
 	@Autowired
-	public DiagnosticTest updateTest(DiagnosticTest test) {
+	public ResponseEntity<DiagnosticTest> updateTest(DiagnosticTest test) throws TestException{
 		if(repo.existsById(test.getId())) {
 			repo.save(test);
-			return test;
+			return new ResponseEntity<DiagnosticTest>(test, HttpStatus.OK);
 		}
-		return null;
+		throw new TestException("Test with the given Id doesn't Exists");
 	}
 	
 	@Autowired
-	public DiagnosticTest removeTest(DiagnosticTest test) {
+	public ResponseEntity<DiagnosticTest> removeTest(DiagnosticTest test) throws TestException {
 		if(repo.existsById(test.getId())) {
 			repo.deleteById(test.getId());
-			return test;
+			return new ResponseEntity<DiagnosticTest>(test, HttpStatus.OK);
+			
 		}
-		return null;
+		throw new TestException("Test with the given Id doesn't Exists");
 	}
 	
 	@Autowired
-	public List<DiagnosticTest> viewAllTest(String criteria){
-		return repo.viewAllTest(criteria);
+	public ResponseEntity<List<DiagnosticTest>> viewAllTest(String criteria) throws TestException{
+		List<DiagnosticTest> d=repo.viewAllTest(criteria);
+		if(d.size()==0) {
+			throw new TestException("Test with the given criteria doesn't Exists");
+		}
+		return new ResponseEntity<List<DiagnosticTest>>(d,HttpStatus.OK);
 	}
 
 }
