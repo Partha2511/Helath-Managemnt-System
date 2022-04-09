9c71330a69274ffe54644149f66d76a69b6d5e6c
PRAGATI <pragatijha2k@gmail.com>
2022-04-09 12:48:10 +0530

    updated DiagnosticTestController and IPatientController class

---
+++ HealthCareSystem/src/main/java/com/cg/hcs/controller/DiagnosticTestController.java
@@ -0,0 +1,60 @@
+package com.cg.hcs.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.cg.hcs.exception.DiagnosticTestException;
+import com.cg.hcs.exception.ErrorInfo;
+import com.cg.hcs.model.DiagnosticTest;
+import com.cg.hcs.service.IDiagnosticTestServiceImpl;
+
+public class DiagnosticTestController {
+	@Autowired
+	IDiagnosticTestServiceImpl impl;
+	
+	@GetMapping("/getAllTests")
+	public ResponseEntity<List<DiagnosticTest>> getAllTest() throws DiagnosticTestException{
+		return impl.getAllTest();
+	}
+	
+	@PostMapping("/addTests")
+	public ResponseEntity<DiagnosticTest> addNewTest(@ RequestBody DiagnosticTest test) throws DiagnosticTestException{
+		return impl.addNewTest(test);	
+	}
+	
+	@GetMapping("/getTests")
+	public ResponseEntity<List<DiagnosticTest>> getTestsOfDiagnosticCenter1(@PathVariable int centerid) throws DiagnosticTestException{
+		return impl.getTestsOfDiagnosticCenter(centerid);
+	
+	}
+	
+	@PutMapping("/updateTests")
+	public ResponseEntity<DiagnosticTest> updateTestDetail(@RequestBody DiagnosticTest test) throws DiagnosticTestException{
+		return impl.updateTestDetail(test);
+	
+	}
+	@DeleteMapping("/removeTests")
+	public ResponseEntity<DiagnosticTest> removeTestFromDiagnosticCenter(@PathVariable int centerid, DiagnosticTest test)throws DiagnosticTestException{
+		return impl.removeTestFromDiagnosticCenter(centerid, test);
+	
+	}
+	
+	
+	@ExceptionHandler(DiagnosticTestException.class)
+	public ResponseEntity<ErrorInfo> handleDiagnosticTestException(DiagnosticTestException e, HttpServletRequest req){
+		ErrorInfo info=new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+		return new ResponseEntity<ErrorInfo>(info, HttpStatus.NOT_FOUND);
+	}
+
+}
---
+++ HealthCareSystem/src/main/java/com/cg/hcs/controller/IPatientController.java
@@ -0,0 +1,56 @@
+package com.cg.hcs.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.cg.hcs.exception.ErrorInfo;
+import com.cg.hcs.exception.PatientException;
+
+import com.cg.hcs.model.Patient;
+import com.cg.hcs.model.TestResult;
+
+
+public class IPatientController {
+	@Autowired
+	IPatientController impl;
+	
+	@PostMapping("/registerPatients")
+	public ResponseEntity<Patient> registerPatient(@RequestBody Patient patient) throws PatientException {
+		return impl.registerPatient(patient);
+	}
+	
+	@PutMapping("/updatePatient")
+	public ResponseEntity<Patient> updatePatientDetails(@RequestBody Patient patient) throws PatientException{
+		return impl.updatePatientDetails(patient);	
+	}
+	
+	@GetMapping("/viewPatient")
+	public ResponseEntity<Patient> viewPatient(@PathVariable String patientUserName) throws PatientException{
+		return impl.viewPatient(patientUserName);	
+	}
+	@GetMapping("/getAllTests")
+	public ResponseEntity<List<TestResult>> getAllTestResult(@PathVariable String patientUserName) throws PatientException{
+		return impl.getAllTestResult(patientUserName);	
+	}
+	
+	@GetMapping("/viewTestResult")
+	public ResponseEntity<TestResult> viewTestResult(@PathVariable int TestresultId) throws PatientException{
+		return impl.viewTestResult(TestresultId);	
+	}
+	
+	@ExceptionHandler(PatientException.class)
+	public ResponseEntity<ErrorInfo> handleDiagnosticCenterException(PatientException e, HttpServletRequest req){
+		ErrorInfo info=new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+		return new ResponseEntity<ErrorInfo>(info, HttpStatus.NOT_FOUND);
+	}
+
+}
